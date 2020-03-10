9c065ad49cf1aa9c55df7fefe51054db420af980
YounesMarwen <younes.maroine@gmail.com>
2020-03-10 12:51:40 +0100

    init js + style

--- main.js
+++ main.js
@@ -114,26 +114,21 @@ const surahsArray = [
     'سورة الفلق',
     'سورة الناس',
 ];
+
 const getAllSurahsNames = async () => {
-    // const allSurahsNames = await fetch('http://api.alquran.cloud/v1/quran/en.asad');
-    // const allSurahsNamesJson = await allSurahsNames.json();
-    // const surahs = await allSurahsNamesJson.data.surahs;
     surahsArray.forEach((e, index) => {
         var div = document.createElement("div");
         div.innerHTML = `<span>${index + 1} - ${e}</span>`;
         div.setAttribute("id", "surah_" + (index + 1));
-        document.querySelector('#list_suras').appendChild(div);
+        document.querySelector('#list_surahs').appendChild(div);
     });
     document.querySelector('.overlay-player').style.display = 'none';
 }
-getAllSurahsNames();
 
+const getSurasByReceiter = async (el, param) => {
 
-const getSurasByReceiter = async (param) => {
-
+    document.querySelector('.overlay-player').style.display = 'flex';
     const getReceiter = async () => {
-
-        document.querySelector('.overlay-player').style.display = 'flex';
         const recitersdata = await fetch('https://mp3quran.net/api/_english.php');
         const recitersJson = await recitersdata.json();
         const receitersArray = recitersJson.reciters;
@@ -144,16 +139,26 @@ const getSurasByReceiter = async (param) => {
                 reciter = e;
             }
         });
-
-
-        document.querySelector('.overlay-player').style.display = 'none';
         return reciter;
     }
     const receiter = await getReceiter();
 
     for (let i = 1; i < 115; i++) {
-        document.querySelector('#surah_' + i).setAttribute("onclick", `readSurah('${receiter.Server}/${lpad(i, 3)}.mp3')`);
+        if (i === 1) {
+            document.querySelector('audio source').src = `${receiter.Server}/${lpad(i, 3)}.mp3`;
+            document.querySelector('audio').load();
+            document.querySelector('#surah_' + i + ' span').classList.add('selected');
+        }
+        document.querySelector('#surah_' + i + ' span').setAttribute("onclick", `readSurah(this ,'${receiter.Server}/${lpad(i, 3)}.mp3')`);
     }
+    let elemsReceiters = document.querySelectorAll('.single-receiter-container span');
+    elemsReceiters.forEach(el => {
+        el.classList.remove('selected');
+    });
+    el.classList.add('selected');
+
+    document.querySelector('.overlay-player').style.display = 'none';
+    document.querySelector('.playlist-surahs-overlay').style.display = 'none';
 }
 
 const lpad = (value, padding) => {
@@ -161,10 +166,93 @@ const lpad = (value, padding) => {
     return (zeroes + value).slice(-padding);
 };
 
-const readSurah = (url) => {
-    console.log(url)
+const readSurah = (el, url) => {
+    document.querySelector(".surahs-list div span.selected").classList.remove('selected');
+    el.classList.add('selected');
     document.querySelector('audio source').src = url;
-
     document.querySelector('audio').load();
     document.querySelector('audio').play();
 }
+
+const appendPlayerContent = () => {
+
+    const html = `<div class="overlay-player">
+<svg xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink" version="1.0" width="64px" height="64px"
+    viewBox="0 0 128 128" xml:space="preserve">
+    <g transform="rotate(0)">
+        <path fill="#afafaf"
+            d="M64.33.25a16 16 0 0 1 16 16c0 4.46-4.93 11.74-9.1 14.62-5.28 3.62-6.56 17.7-6.56 17.7s-.9-13.92-7.24-18.03c-5.4-3.5-9.1-9.8-9.1-14.3a16 16 0 0 1 16-16z" />
+        <path fill="#000000"
+            d="M64.33.25a16 16 0 0 1 16 16c0 4.46-4.93 11.74-9.1 14.62-5.28 3.62-6.56 17.7-6.56 17.7s-.9-13.92-7.24-18.03c-5.4-3.5-9.1-9.8-9.1-14.3a16 16 0 0 1 16-16z"
+            transform="rotate(45 64 64)" />
+        <path fill="#000000"
+            d="M64.33.25a16 16 0 0 1 16 16c0 4.46-4.93 11.74-9.1 14.62-5.28 3.62-6.56 17.7-6.56 17.7s-.9-13.92-7.24-18.03c-5.4-3.5-9.1-9.8-9.1-14.3a16 16 0 0 1 16-16z"
+            transform="rotate(90 64 64)" />
+        <path fill="#000000"
+            d="M64.33.25a16 16 0 0 1 16 16c0 4.46-4.93 11.74-9.1 14.62-5.28 3.62-6.56 17.7-6.56 17.7s-.9-13.92-7.24-18.03c-5.4-3.5-9.1-9.8-9.1-14.3a16 16 0 0 1 16-16z"
+            transform="rotate(135 64 64)" />
+        <path fill="#000000"
+            d="M64.33.25a16 16 0 0 1 16 16c0 4.46-4.93 11.74-9.1 14.62-5.28 3.62-6.56 17.7-6.56 17.7s-.9-13.92-7.24-18.03c-5.4-3.5-9.1-9.8-9.1-14.3a16 16 0 0 1 16-16z"
+            transform="rotate(180 64 64)" />
+        <path fill="#000000"
+            d="M64.33.25a16 16 0 0 1 16 16c0 4.46-4.93 11.74-9.1 14.62-5.28 3.62-6.56 17.7-6.56 17.7s-.9-13.92-7.24-18.03c-5.4-3.5-9.1-9.8-9.1-14.3a16 16 0 0 1 16-16z"
+            transform="rotate(225 64 64)" />
+        <path fill="#000000"
+            d="M64.33.25a16 16 0 0 1 16 16c0 4.46-4.93 11.74-9.1 14.62-5.28 3.62-6.56 17.7-6.56 17.7s-.9-13.92-7.24-18.03c-5.4-3.5-9.1-9.8-9.1-14.3a16 16 0 0 1 16-16z"
+            transform="rotate(270 64 64)" />
+        <path fill="#000000"
+            d="M64.33.25a16 16 0 0 1 16 16c0 4.46-4.93 11.74-9.1 14.62-5.28 3.62-6.56 17.7-6.56 17.7s-.9-13.92-7.24-18.03c-5.4-3.5-9.1-9.8-9.1-14.3a16 16 0 0 1 16-16z"
+            transform="rotate(315 64 64)" />
+        <animateTransform attributeName="transform" type="rotate"
+            values="0 64 64;45 64 64;90 64 64;135 64 64;180 64 64;225 64 64;270 64 64;315 64 64"
+            calcMode="discrete" dur="720ms" repeatCount="indefinite" />
+    </g>
+</svg>
+</div>
+<h1>Quran player</h1>
+<div class="receiters-list">
+<h3 align="center">Receiters</h3>
+<div>
+    <div class="single-receiter-container" style="border-right: solid 1px white;"><span
+            class="first-receiter" onclick="getSurasByReceiter(this, 49)">Abdelbari
+            Al-Toubayti</span>
+    </div>
+    <div class="single-receiter-container" style="border-right: solid 1px white;"><span
+            class="second-receiter" onclick="getSurasByReceiter(this, 55)">Abdul Aziz Al-Ahmad</span>
+    </div>
+    <div class="single-receiter-container" style="border-right: solid 1px white;"><span
+            class="third-receiter" onclick="getSurasByReceiter(this, 53)">Abdulbasit
+            Abdulsamad</span>
+    </div>
+    <div class="single-receiter-container"><span class="fourth-receiter"
+            onclick="getSurasByReceiter(this, 62)">Abdullah Al-Johany</span>
+    </div>
+</div>
+</div>
+<div class="playlist-container">
+
+<div class="playlist-surahs">
+    <div class="playlist-surahs-overlay"><span>PLEASE SELECT A RECEITER</span></div>
+    <h3 align="center">Surahs</h3>
+    <div id="list_surahs" class="surahs-list"></div>
+</div>
+<div class="player-container">
+    <audio controls>
+        <source src="" type="audio/mpeg">
+    </audio>
+</div>
+</div>`;
+    document.querySelector('.quran-player-container').innerHTML = html;
+
+}
+
+const init = () => {
+    appendPlayerContent();
+    getAllSurahsNames();
+    let el = document.querySelector('.first-receiter')
+    getSurasByReceiter(el, 49);
+
+}
+init();
+
